57cf497aefc84a4e8911c81a6ffe11539cd65155
yujvv <106058346+yujvv@users.noreply.github.com>
2024-03-12 18:48:09 +0900

    Update loader.py with text loader

--- loader.py
+++ loader.py
@@ -10,14 +10,36 @@
 
 # web_links = ["https://www.databricks.com/","https://help.databricks.com","https://databricks.com/try-databricks","https://help.databricks.com/s/","https://docs.databricks.com","https://kb.databricks.com/","http://docs.databricks.com/getting-started/index.html","http://docs.databricks.com/introduction/index.html","http://docs.databricks.com/getting-started/tutorials/index.html","http://docs.databricks.com/release-notes/index.html","http://docs.databricks.com/ingestion/index.html","http://docs.databricks.com/exploratory-data-analysis/index.html","http://docs.databricks.com/data-preparation/index.html","http://docs.databricks.com/data-sharing/index.html","http://docs.databricks.com/marketplace/index.html","http://docs.databricks.com/workspace-index.html","http://docs.databricks.com/machine-learning/index.html","http://docs.databricks.com/sql/index.html","http://docs.databricks.com/delta/index.html","http://docs.databricks.com/dev-tools/index.html","http://docs.databricks.com/integrations/index.html","http://docs.databricks.com/administration-guide/index.html","http://docs.databricks.com/security/index.html","http://docs.databricks.com/data-governance/index.html","http://docs.databricks.com/lakehouse-architecture/index.html","http://docs.databricks.com/reference/api.html","http://docs.databricks.com/resources/index.html","http://docs.databricks.com/whats-coming.html","http://docs.databricks.com/archive/index.html","http://docs.databricks.com/lakehouse/index.html","http://docs.databricks.com/getting-started/quick-start.html","http://docs.databricks.com/getting-started/etl-quick-start.html","http://docs.databricks.com/getting-started/lakehouse-e2e.html","http://docs.databricks.com/getting-started/free-training.html","http://docs.databricks.com/sql/language-manual/index.html","http://docs.databricks.com/error-messages/index.html","http://www.apache.org/","https://databricks.com/privacy-policy","https://databricks.com/terms-of-use"]
 
+# 华为光产品公开安装文档
+# https://support.huawei.com/enterprise/zh/optical-access/smartax-ma5600t-pid-18133
+
 # loader = WebBaseLoader(web_links)
 # documents = loader.load()
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
+# https://python.langchain.com/docs/modules/data_connection/document_transformers/
+
+# 从文件中读取文本
+file_path = "hg8346m-olt.txt"
+
+with open(file_path, "r", encoding="utf-8") as file:
+    text = file.read()
+
+# 将文本加载到 documents 中
+# documents.append(text)
 
 # Splitting in Chunks using Text Splitters
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-all_splits = text_splitter.split_documents(documents)
+
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+# all_splits = text_splitter.split_documents(text)
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=0
+)
+texts = text_splitter.split_text(text)
+
+print("------------", texts)
 
 # Creating Embeddings and Storing in Vector Store
 # You have to create embeddings for each small chunk of text and store them in the vector store (i.e. FAISS). You will be using `all-mpnet-base-v2` Sentence Transformer to convert all pieces of text in vectors while storing them in the vector store.
