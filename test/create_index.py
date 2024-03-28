f591bf89ea2fb72dd9ada040012ed7fc9734812a
Your Name <you@example.com>
2024-03-28 21:15:14 +0900

    update demo

--- test/create_index.py
+++ test/create_index.py
@@ -1,24 +1,23 @@
 from langchain_community.vectorstores import FAISS
 from utils import load_documents, save_db, load_embeddings
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+model_name = "BAAI/bge-large-en-v1.5"
+model_kwargs = {'device': 'cuda'}
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+model = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs,
+    query_instruction="为这个句子生成表示以用于检索相关文章："
+)
+# model.query_instruction = "为这个句子生成表示以用于检索相关文章："
+embedding_function = model
+# embedding_function = load_embeddings()
 
-embedding_function = load_embeddings()
 documents = load_documents("data/")
 
 db = FAISS.from_documents(documents, embedding_function)
 print("Index Created")
 save_db(db)
 
-print(db.similarity_search("ISO/IEC 27001 standard"))
-
-
-# from langchain.embeddings import HuggingFaceBgeEmbeddings
-# model_name = "BAAI/bge-large-en-v1.5"
-# model_kwargs = {'device': 'cuda'}
-# encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
-# model = HuggingFaceBgeEmbeddings(
-#     model_name=model_name,
-#     model_kwargs=model_kwargs,
-#     encode_kwargs=encode_kwargs,
-#     query_instruction="为这个句子生成表示以用于检索相关文章："
-# )
-# model.query_instruction = "为这个句子生成表示以用于检索相关文章："
+print(db.similarity_search("huawei f50"))
