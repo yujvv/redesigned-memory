b0ecf03655880172b871af1f330a50b392665835
yujvv <106058346+yujvv@users.noreply.github.com>
2024-04-01 16:14:13 +0900

    Add files via upload

---
+++ test/flask.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+
+from utils import load_embeddings, load_db
+
+app = Flask(__name__)
+
+
+
+embedding_function = load_embeddings()
+
+db = load_db(embedding_function)
+
+
+@app.route('/ask', methods=['POST'])
+def ask_question():
+    data = request.get_json()
+    query = data['query']
+    context = db.similarity_search(query)
+    return jsonify({"context": context})
+
+if __name__ == "__main__":
+    # my ip
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+# import requests
+
+# url = 'http://<flask_app_ip>:5000/ask'
+# question = 'Your question goes here'
+
+# response = requests.post(url, json={'query': query})
+# data = response.json()
+
+# print("context:", data['context'])
