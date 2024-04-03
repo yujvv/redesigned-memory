53b0fbddacbf2907897d5621d738fbe06aaa5032
yujvv <106058346+yujvv@users.noreply.github.com>
2024-04-03 17:47:05 +0900

    update app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 from PIL import Image
 import io
-from api.faiss_api import Faiss_GPU
+from api.faiss_session import SessionRetriever
+from api.UD import UniqueDictionary
+from sentence_transformers import SentenceTransformer
 
 
-faiss_gpu = Faiss_GPU("my_index", "/demo/index")
+# Initialize the vector database
+EMBEDDING_PATH = "D:/Yu/rag/bge-large-zh-v1.5"
+model = SentenceTransformer(EMBEDDING_PATH)
+retriever = SessionRetriever(model)
+uid = UniqueDictionary()
 
 class ImageTextUploader:
     def __init__(self):
@@ -66,8 +72,11 @@ class ImageTextUploader:
 
         if st.button("Send"):
             st.session_state.dialogue_history.append(("User", input_text))
-            # Add your logic to process the input text and generate a response
-            response = "Response"
+            # The logic to process the input text and generate a response
+            results = retriever.query_index(input_text)
+            print("Query Result:", results)
+
+            response = results[0][1][1]
             st.session_state.dialogue_history.append(("💬", response))
 
         if st.button("Clear"):
@@ -84,65 +93,62 @@ def main():
     uploader = ImageTextUploader()
     uploader.upload_sidebar()
 
-    # Initialize the vector database
 
-
-    # # Add some data
-    # faiss_gpu.add({"apple": 0, "banana": 1, "orange": 2})
-
-    # # Query for similar items
-
-    # results = faiss_gpu.query("orange")
-    # print(results)  # [(0.8, 0), (0.7, 1), (0.6, 2)]
-
-    # # Delete an item
-    # faiss_gpu.delete("apple")
+# query = 'Find and replace specific text in a file'
+# relevant_actions = retriever.query_index(query)
+# for action, semantic, score in relevant_actions:
+#     print(f'Action: {action}, Semantic: {semantic}, Score: {score}')
 
 
     # Add image annotations and text to the vector database
     for caption in uploader.captions:
-        # vector_db.add(caption)
-        print("----------caption", caption)
-    for chunk_num, text in st.session_state.text_blocks:
-        # vector_db.add(text)
-        print("----------text", text)
-        print("----------chunk_num", chunk_num)
+        temp_uid = uid.add_element(caption)
+        retriever.update_index(temp_uid)
+        # print("----------caption", caption)
+    for text in st.session_state.text_blocks:
+        temp_uid = uid.add_element(text)
+        retriever.update_index(temp_uid)
+        # print("----------text", text)
+
 
 
     # --------------------
     uploader.display_content()
 
-    # def chat_interface():
-    #     st.header("UniConvo 💬")
-    #     input_text = st.text_input("Enter Your Message")
 
-    #     if st.button("Send"):
-    #         uploader.dialogue_history.append(("User", input_text))
+    def chat_interface():
+        st.header("UniConvo 💬")
+        input_text = st.text_input("Enter Your Message")
 
-    #         # Search the vector database
-    #         results = vector_db.search(input_text, k=3)
+        if st.button("Send"):
+            uploader.dialogue_history.append(("User", input_text))
 
-    #         # Construct the prompt based on context reasoning
-    #         context = []
-    #         for result in results:
-    #             text, distance = result
-    #             if text in uploader.captions:
-    #                 image_index = uploader.captions.index(text) + 1
-    #                 context.append(f"Image {image_index}: {text}")
-    #             elif (chunk_num, text) in uploader.text_blocks:
-    #                 text_index = uploader.text_blocks.index((chunk_num, text)) + 1
-    #                 context.append(f"Text {text_index}: {text}")
-    #             else:
-    #                 context.append(text)
+            # Search the vector database
+            results = retriever.query_index(input_text)
+            print("Query Result:", results)
 
-    #         context_str = "\n".join(context)
-    #         prompt = f"Given the context:\n\n{context_str}\n\nAnswer the following question: {input_text}"
+            # # Construct the prompt based on context reasoning
+            # context = []
+            # for result in results:
+            #     text, distance = result
+            #     if text in uploader.captions:
+            #         image_index = uploader.captions.index(text) + 1
+            #         context.append(f"Image {image_index}: {text}")
+            #     elif (chunk_num, text) in uploader.text_blocks:
+            #         text_index = uploader.text_blocks.index((chunk_num, text)) + 1
+            #         context.append(f"Text {text_index}: {text}")
+            #     else:
+            #         context.append(text)
 
-    #         # Request the language model (you'll need to implement this part)
-    #         # todo
-    #         response = request_language_model(prompt)
+            # context_str = "\n".join(context)
+            # prompt = f"Given the context:\n\n{context_str}\n\nAnswer the following question: {input_text}"
 
-    #         uploader.dialogue_history.append(("💬", response))
+            # # Request the language model (you'll need to implement this part)
+            # # todo
+            # response = request_language_model(prompt)
+
+            # response = results
+            # uploader.dialogue_history.append(("💬", response))
 
         # if st.button("Clear"):
         #     uploader.dialogue_history = []
