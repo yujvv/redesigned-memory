bb860017c8c67f03b6c4cc5624725fe1ec34f3de
yujvv <106058346+yujvv@users.noreply.github.com>
2024-05-15 17:23:14 +0900

    Add files via upload

--- demo_car/flask_demo.py
+++ demo_car/flask_demo.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from api.faiss_api import Faiss_GPU
 from api.loader_docx import Loader
+from datetime import datetime
 # from api.chatglm import ChatGLMInterface
 
 app = Flask(__name__)
@@ -17,19 +18,22 @@ def build_dict(content_list):
 
 # Initialize models
 loader = Loader()
-docx_file = '2test.docx'
+docx_file = 'M9.docx'
 content_list = loader.extract_content(docx_file)
 result_dict, title_dict = build_dict(content_list)
-faiss_gpu = Faiss_GPU("2test.docx", "./index")
+faiss_gpu = Faiss_GPU("index2", "./index")
 faiss_gpu.add(result_dict)
+
 # language_model_interface = ChatGLMInterface()
 
 
 # Function to process input and return response
 def process_input(user_input):
     results = faiss_gpu.query_index(user_input, result_dict, title_dict)
+    # print("------------", results)
     top = results[0][0]
     title = results[0][3]
+
     # prompt = f"你好，你是我的车内助手。请基于背景，帮我温柔地回答问题。\nContext:{top}\nQ: {user_input}\nA:"
     # output = language_model_interface.generate_response(prompt, False)
 
@@ -45,9 +49,17 @@ def process_input(user_input):
 # Route for receiving text input and returning processed text and image name
 @app.route('/process_text', methods=['POST'])
 def process_text():
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
     data = request.get_json()
     user_input = data['user_input']
+
+    print("Time_________", formatted_time)
+    print("Input______", user_input)
     top, title = process_input(user_input)
+    print("Context_________", top, "\nTitle_________", title)
+
     bot_response, image_name = top, title
 
     return jsonify({'bot_response': bot_response, 'image_name': image_name})
